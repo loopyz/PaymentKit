286d4c16dc1bd3f3d5b8ccbd31403d431c937993
loopyz <lguo@andrew.cmu.edu>
2014-10-01 00:56:56 -0700

    added macro

--- PaymentKit/PTKView.m
+++ PaymentKit/PTKView.m
@@ -7,6 +7,7 @@
 //
 
 #define RGB(r,g,b) [UIColor colorWithRed:r/255.0f green:g/255.0f blue:b/255.0f alpha:1.0f]
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 #define DarkGreyColor RGB(0,0,0)
 #define RedColor RGB(253,0,17)
 #define DefaultBoldFont [UIFont boldSystemFontOfSize:17]
